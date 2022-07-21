840c8381ac710c6fac9a15c7649f908e938b35e3
HammadASiddiqui <hammadahmed789@gmail.com>
2022-07-21 17:11:23 +0500

    stream done logger added - refactored [done]

--- producer/producer.py
+++ producer/producer.py
@@ -7,6 +7,17 @@ from json import dumps
 from kafka import KafkaProducer
 import logging
 
+<<<<<<< HEAD
+=======
+logging.basicConfig(filename="producer.log", 
+					format='%(asctime)s %(message)s', 
+					filemode='w') 
+
+logger=logging.getLogger() 
+
+logger.setLevel(logging.DEBUG)
+
+>>>>>>> 17569e5 (basic insertion with consumer [done])
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
     value_serializer=lambda x: dumps(x).encode('utf-8')
@@ -15,14 +26,28 @@ producer = KafkaProducer(
 #process tweets from pagenated data
 def process_page(page_results):
     for i, tweet in enumerate(page_results):
+<<<<<<< HEAD
         print("Sending>>>>>>>>>>>>>>>tweet#"+str(i))
+=======
+        logger.info("Sending>>>>>>>>>>>>>>>tweet#"+str(i))
+>>>>>>> 17569e5 (basic insertion with consumer [done])
         producer.send('tweets_topic', value= tweet._json)
         
 if __name__ == '__main__':
     print('application started')
+<<<<<<< HEAD
     with open('api_auth.yaml') as f:
         keys = yaml.load(f, Loader=SafeLoader)
     
+=======
+
+    try:
+        with open('api_auth.yaml') as api_auth_file:
+            keys = yaml.load(api_auth_file, Loader=SafeLoader)
+    except:
+        logger.error("api_auth.yaml not found")
+
+>>>>>>> 17569e5 (basic insertion with consumer [done])
     OAUTH_KEYS = {'consumer_key': keys['consumer_key'], 'consumer_secret': keys['consumer_secret'],
                   'access_token_key': keys['access_token_key'], 'access_token_secret': keys['access_token_secret']}
     auth = tweepy.OAuthHandler(
